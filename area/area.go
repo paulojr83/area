a2d29e5c27a6dcf9c3a07f45d27caf47072bb669
Paulo Porto <jrpauloporto@gmail.com>
2020-12-04 13:14:41 -0300

    adicionando pastas

--- area/area.go
+++ area/area.go
@@ -1,6 +1,7 @@
 package area
 
 import "math"
+
 // Pi é uma proporção numérica definida pela relação entre o perímetro de uma circunferência e seu diâmetro
 const Pi = 3.1416
 
@@ -15,6 +16,6 @@ func Rect(base, altura float64) float64 {
 }
 
 // Não é visível
-func _TrianguloEq(base, altura float64) float64  {
-	return ( base * altura ) / 2
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
 }
